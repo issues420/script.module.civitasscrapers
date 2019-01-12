211523f638f63d72a6fcf649364cf1cab9f4debd
Clownman <44384002+13Clowns@users.noreply.github.com>
2019-01-12 08:13:31 -0800

    merge

--- lib/civitasscrapers/sources/playmovies.py
+++ lib/civitasscrapers/sources/playmovies.py
@@ -1,8 +1,26 @@
-# -*- coding: UTF-8 -*-
-# -Cleaned and Checked on 12-21-2018 by JewBMX in Scrubs.
+# -*- coding: utf-8 -*-
+
+'''
+    Eggman Add-on
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 import re
-from resources.lib.modules import cleantitle,source_utils,debrid,cfscrape
+from resources.lib.modules import cleantitle
+from resources.lib.modules import cfscrape
+from resources.lib.modules import source_utils
 
 
 class source:
@@ -13,7 +31,6 @@ class source:
         self.base_link = 'http://playmovies.es'
         self.search_link = '/%s'
 
-
     def movie(self, imdb, title, localtitle, aliases, year):
         try:
             title = cleantitle.geturl(title)
@@ -22,11 +39,9 @@ class source:
         except:
             return
 
-
     def sources(self, url, hostDict, hostprDict):
         try:
             sources = []
-            if debrid.status() == False: raise Exception()
             scraper = cfscrape.create_scraper()
             r = scraper.get(url).content
             try:
@@ -47,7 +62,5 @@ class source:
             return
         return sources
 
-
     def resolve(self, url):
         return url
-
